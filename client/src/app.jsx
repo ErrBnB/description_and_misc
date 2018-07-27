3100fcebab5ad1940daa67e78eff06f51024ffb6
Hiroaki Oshima <kifa0422@gmail.com>
2018-07-26 22:38:17 -0700

    able to display entry points

--- client/src/app.jsx
+++ client/src/app.jsx
@@ -1,4 +1,9 @@
-import React from 'react';
 class App extends React.Component {
-
+  render() {
+    return (
+      <div>Hello World!....I should show up!</div>
+    );
+  }
 }
+
+window.App = App;
