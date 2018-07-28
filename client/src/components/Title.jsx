5393b9a90554c8ef0c348732bcf11da3655cc05f
Hiroaki Oshima <kifa0422@gmail.com>
2018-07-28 04:10:11 -0700

    redesigned title html

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -23,9 +23,7 @@ class House_type extends React.Component {
     }
     render () {
         return (
-          <a className="_1biqilc" href="/s/Makawao--HI?listing_types[]=60">
-            <div className="_4efw5a"><span><span className="_1hh2h7tb"><span style={{color: '#734f21'}}>Entire cottage</span></span></span></div>
-          </a>
+            <div className="type-box"><span><span className="type-text"><span style={{color: '#734f21'}}>Entire cottage</span></span></span></div>
         )
     }
 }
@@ -37,10 +35,9 @@ class Main_title extends React.Component {
     render () {
         return (
           <div>
-            <div className="_2h22gn">
+            <div className="main-title">
                 <Title_Box />
                 <Owner_Box />
-
             </div>
           </div>
         )
@@ -53,22 +50,18 @@ class Title_Box extends React.Component {
     }
     render () {
         return (
-        <div className="_1hpgssa1">
+        <div className="title-box">
             <div style={{marginBottom: 6}}>
               <div itemProp="name">
                 <span dir="ltr">
-                  <span className="_12ei9u44">
-                    <h1 tabIndex={-1} className="_1xu9tpch">Adorable Garden Gingerbread House</h1>
+                  <span className="main-title-text">
+                    <h1 tabIndex={-1} className="main-title-bold">Adorable Garden Gingerbread House</h1>
                   </span>
                 </span>
               </div>
             </div>
             <div style={{marginBottom: 16}}>
-              <div data-location="Makawao">
-                <a href="#neighborhood" className="_1biqilc">
-                  <div className="_ncwphzu">Makawao</div>
-                </a>
-              </div>
+                  <div className="location">Makawao</div>
             </div>
           </div>
         )
@@ -82,23 +75,15 @@ class Owner_Box extends React.Component {
 
     render () {
         return (
-            <div className="_10pi7ah2">
+            <div className="owner-box">
                 <div style={{marginTop: 12}}>
                 <div className="text-center text-muted">
                     <div style={{marginBottom: 6}}>
-                    <div className="_1ij6gln6" style={{height: 64, width: 64, display: 'block'}}>
-                        <a href="/users/show/3280543" target="_blank" rel="noopener noreferrer" className="_110nrr2" aria-label="Eve User Profile" aria-busy="false"><img className="_12r18es" src="https://a0.muscache.com/im/users/3280543/profile_pic/1378257191/original.jpg?aki_policy=profile_x_medium" height={64} width={64} alt="Eve User Profile" title="Eve User Profile" /></a>
-                        <div className="_8kd6yy">
-                        <svg viewBox="0 0 14 24" role="img" aria-label="Eve is a superhost." focusable="false" style={{height: 32, width: 32, display: 'block', fill: 'currentColor'}}>
-                            <path d="m10.5 20.0005065c0 1.9326761-1.56704361 3.4994935-3.5 3.4994935s-3.5-1.5668174-3.5-3.4994935c0-1.9326762 1.5670426-3.5005065 3.5-3.5005065s3.5 1.5678303 3.5 3.5005065m-9.99486248-18.58757644-.00513752 8.13836018c0 .45796416.21682079.88992936.58880718 1.17090736l5.07730539 3.831699c.4870761.367971 1.16836618.367971 1.65647028.0009994l5.08141685-3.8266984c.3719859-.2789784.5898342-.7109444.5908612-1.16790827.0010271-1.75186288.0041101-6.21051146.0051391-8.14035983 0-.50396002-.4202834-.91292822-.9392158-.91292822l-11.11643181-.00699945c-.51790391-.00099942-.93818728.40796878-.93921487.91292823" fill="#fff" />
-                            <path d="m12 9.5-5-3.70124468 5-3.79875532zm-6.1292309 9.187485c-.52182677.3180834-.8707691.8762459-.8707691 1.5144379 0 .9937534.83703449 1.7980771 1.870162 1.7980771.81806646 0 1.50434636-.5065007 1.75946763-1.2095239z" fill="#ffb400" />
-                            <path d="m12 9.5-5 3.75-5-3.75v-7.5z" fill="#ff5a5f" />
-                            <path d="m7 24c-2.2060547 0-4-1.7939453-4-3.9990234 0-2.2060547 1.7939453-4.0009766 4-4.0009766s4 1.7949219 4 4.0009766c0 2.2050781-1.7939453 3.9990234-4 3.9990234zm0-7c-1.6542969 0-3 1.3466797-3 3.0009766 0 1.6533203 1.3457031 2.9990234 3 2.9990234s3-1.3457031 3-2.9990234c0-1.6542969-1.3457031-3.0009766-3-3.0009766zm.0039062-1.8242188c-.4560547 0-.9121094-.1064453-1.2617188-.3164062l-5.0458984-3.8642578c-.4697265-.3642578-.696289-.8525391-.696289-1.4951172v-8c.0009766-.3730469.1679688-.7529297.4580078-1.0429688.2900391-.2905273.6689453-.4570312 1.0410156-.4570312h.0019531 10.9990235c.7851562 0 1.5.7148438 1.5 1.5v7.9277344c-.0009766.6762695-.2421875 1.2177734-.6953125 1.5668945l-5.0009766 3.8325195c-.3505859.2333985-.8251953.3486328-1.2998047.3486328zm-5.5058593-14.1757812c-.1044922 0-.2324219.0625-.3330078.1635742-.1015625.1020508-.1650391.230957-.1650391.3374024v7.9990234c0 .3305664.0888672.5341797.3066406.703125l4.9970703 3.8310547c.3330078.1953125 1.0859375.2001953 1.4208984-.0205078l4.9716797-3.8125c.2001954-.1542969.3027344-.4155274.303711-.7749024v-7.9267578c0-.2285156-.2714844-.4995117-.5-.4995117h-11-.0009766s0 0-.0009765 0z" fill="#484848" />
-                        </svg>
-                        </div>
+                    <div className="owner-image-box" style={{height: 64, width: 64, display: 'block'}}>
+                        <img className="owner-image" src="https://a0.muscache.com/im/users/3280543/profile_pic/1378257191/original.jpg?aki_policy=profile_x_medium" height={64} width={64} alt="Eve User Profile" title="Eve User Profile" />
                     </div>
                     </div>
-                    <div className="_tzpi7ec">Eve</div>
+                    <div className="owner-name">Eve</div>
                 </div>
                 </div>
             </div>
@@ -129,12 +114,12 @@ class Info1 extends React.Component {
     }
     render () {
         return (
-            <div className="_36rlri" style={{marginRight: 16}}>
-                <div className="_qtix31">
-                <div className="_1thk0tsb">
-                    <div style={{marginRight: 8}}><span className="_12i0h32r"><span className="_8tbpu3" aria-hidden="true">󰄂</span></span></div>
+            <div className="info" style={{marginRight: 16}}>
+                <div className="info-middle">
+                <div className="info-text-box">
+                    <div style={{marginRight: 8}}><span className="info-svg"><span className="airmoji" aria-hidden="true">󰄂</span></span></div>
                 </div>
-                <div className="_1thk0tsb"><span className="_fgdupie">2 guests</span></div>
+                <div className="info-text-box"><span className="info-text">2 guests</span></div>
                 </div>
             </div>
         )
@@ -147,12 +132,12 @@ class Info2 extends React.Component {
     }
     render () {
         return (
-            <div className="_36rlri" style={{marginRight: 16}}>
-                <div className="_qtix31">
-                <div className="_1thk0tsb">
-                    <div style={{marginRight: 8}}><span className="_12i0h32r"><span className="_8tbpu3" aria-hidden="true">󰄄</span></span></div>
+            <div className="info" style={{marginRight: 16}}>
+                <div className="info-middle">
+                <div className="info-text-box">
+                    <div style={{marginRight: 8}}><span className="info-svg"><span className="airmoji" aria-hidden="true">󰄄</span></span></div>
                 </div>
-                <div className="_1thk0tsb"><span className="_fgdupie">Studio</span></div>
+                <div className="info-text-box"><span className="info-text">Studio</span></div>
                 </div>
             </div>
         )
@@ -165,12 +150,12 @@ class Info3 extends React.Component {
     }
     render () {
         return (
-            <div className="_36rlri" style={{marginRight: 16}}>
-                <div className="_qtix31">
-                <div className="_1thk0tsb">
-                    <div style={{marginRight: 8}}><span className="_12i0h32r"><span className="_8tbpu3" aria-hidden="true">󰄃</span></span></div>
+            <div className="info" style={{marginRight: 16}}>
+                <div className="info-middle">
+                <div className="info-text-box">
+                    <div style={{marginRight: 8}}><span className="info-svg"><span className="airmoji" aria-hidden="true">󰄃</span></span></div>
                 </div>
-                <div className="_1thk0tsb"><span className="_fgdupie">1 bed</span></div>
+                <div className="info-text-box"><span className="info-text">1 bed</span></div>
                 </div>
             </div>
         )
@@ -183,12 +168,12 @@ class Info4 extends React.Component {
     }
     render () {
         return (
-            <div className="_36rlri" style={{marginRight: 0}}>
-                <div className="_qtix31">
-                <div className="_1thk0tsb">
-                    <div style={{marginRight: 8}}><span className="_12i0h32r"><span className="_8tbpu3" aria-hidden="true">󰄁</span></span></div>
+            <div className="info" style={{marginRight: 0}}>
+                <div className="info-middle">
+                <div className="info-text-box">
+                    <div style={{marginRight: 8}}><span className="info-svg"><span className="airmoji" aria-hidden="true">󰄁</span></span></div>
                 </div>
-                <div className="_1thk0tsb"><span className="_fgdupie">1 bath</span></div>
+                <div className="info-text-box"><span className="info-text">1 bath</span></div>
                 </div>
             </div>
         )
